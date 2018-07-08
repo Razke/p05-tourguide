002cee7c64d2d483cad2d41ec35fdf30753a2d10
razscu <razvan.dinculescu@outlook.com>
2018-07-09 02:38:34 +0300

    Add the locations for the Food & Drink fragment

--- app/src/main/java/com/example/raz/p05_tourguide/FoodDrinkFragment.java
+++ app/src/main/java/com/example/raz/p05_tourguide/FoodDrinkFragment.java
@@ -18,8 +18,11 @@ public class FoodDrinkFragment extends Fragment {
 
         // Create a list of locations
         final ArrayList<Location> locations = new ArrayList<Location>();
-        locations.add(new Location(getString(R.string.test1), getString(R.string.test2), R.drawable.image_poster_min));
-        locations.add(new Location(getString(R.string.test1), getString(R.string.test2), R.drawable.second_image_poster_min));
+        locations.add(new Location("Rausch Schokoladenhaus", getString(R.string.food_drink_schokoladenhaus_desc), R.drawable.image_food_drink_schokoladenhaus));
+        locations.add(new Location("Marjellchen", getString(R.string.food_drink_marjellchen_desc), R.drawable.image_food_drink_marjellchen));
+        locations.add(new Location("L'Osteria", getString(R.string.food_drink_losteria_desc), R.drawable.image_food_drink_losteria));
+        locations.add(new Location("Jolly", getString(R.string.food_drink_jolly_desc), R.drawable.image_food_drink_jolly));
+        locations.add(new Location("Ristorante a Mano", getString(R.string.food_drink_ristoranteamano_desc), R.drawable.image_food_drink_ristoranteamano));
 
         LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
 
